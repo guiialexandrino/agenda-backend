e26748d860017157317a13a2496d57e86930f9dc
Guilherme <guiialexandrino@gmail.com>
2023-02-09 20:09:49 -0300

    Arrumando pastas

---
+++ src/app.js
@@ -0,0 +1,33 @@
+require('dotenv').config();
+const path = require('node:path');
+const express = require('express');
+const app = express();
+const apiEnter = require('../routes/enter.js');
+const apiUser = require('../routes/api-user.js');
+const mongoose = require('mongoose');
+const cors = require('cors');
+
+mongoose.set('strictQuery', true);
+mongoose.connect(
+  process.env.MONGO_CONNECTION_URL,
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  },
+  (error) => {
+    if (error) console.log(error);
+    else console.log('Banco carregado...');
+  }
+);
+
+app.use(cors({ exposedHeaders: ['authorization-token'] }));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use('/enter', apiEnter);
+app.use('/api', apiUser);
+app.use('/uploads', express.static(path.resolve(__dirname, '.', 'uploads')));
+
+app.listen(process.env.PORT, () => {
+  console.log(`Servidor rodando na porta: ${process.env.PORT}`);
+});
