26c6118839ebf1448eaf3529ccb4a24f5d73abed
Guilherme <guiialexandrino@gmail.com>
2023-02-09 14:03:04 -0300

    Updates Basicos

--- models/User.js
+++ models/User.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose');
 
 const userSchema = mongoose.Schema({
   name: { type: String, required: true, minlength: 3, maxlength: 50 },
+  avatar: { type: String },
   email: { type: String, required: true, minlength: 3, maxlength: 100 },
   password: { type: String, required: true, minlength: 6, maxlength: 100 },
   createdAt: { type: Date, default: Date.now },
@@ -13,6 +14,7 @@ userSchema.methods.toJSON = function () {
   return {
     id: this._id,
     name: this.name,
+    avatar: this.avatar,
     email: this.email,
     createdAt: this.createdAt,
     lastLoginAt: this.lastLoginAt,
